6af79e09d0e6c7373cd04308bcbb7345117e918b
KibalinMax <kibalaspb@mail.ru>
2023-08-07 11:41:34 +0300

    change2

--- src/channel.py
+++ src/channel.py
@@ -1,4 +1,15 @@
+import json
+import os
+from googleapiclient.discovery import build
+import isodate
 
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -9,4 +20,6 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel_id = self.channel_id
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        printj(channel)
